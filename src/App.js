68d79d8d438db106f7c9843e4729fb38bd4d5b73
Rokas <rookas.rudzenskas@gmail.com>
2021-05-21 18:31:32 +0300

    Roaks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello 🚀</h1>
+      <Header />
+
+    {/*     Header  */}
+
+    {/*    App body     */}
+
+        {/*    Sidebar */}
+        {/*    Feed*/}
+        {/*  Widgets      */}
+
+
     </div>
   );
 }
