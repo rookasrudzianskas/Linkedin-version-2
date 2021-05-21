b7b0daef97f16d90c437746e7b34ae1eb19db82c
Rokas <rookas.rudzenskas@gmail.com>
2021-05-21 18:36:03 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import "./styles/Header.css";
+import SearchIcon from '@material-ui/icons/Search';
 
 
 const Header = () => {
     return (
         <div className="header">
             <div className="header__left">
-                <img src="" alt=""/>
+                <img src="https://www.amocrm.com/static/images/pages/integrations/logo/linkedin.png" alt=""/>
 
                 <div className="header__search">
+                    <SearchIcon />
                     <input type="text"/>
                 </div>
             </div>
