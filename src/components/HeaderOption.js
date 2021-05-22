6602be5b2c81a748bec98dbd1b79256184033d75
Rokas <rookas.rudzenskas@gmail.com>
2021-05-22 17:46:35 +0300

    Roaks

--- src/components/HeaderOption.js
+++ src/components/HeaderOption.js
@@ -5,7 +5,9 @@ import {Avatar} from "@material-ui/core";
 const HeaderOption = ({avatar, title, Icon}) => {
     return (
         <div className="headerOption">
+            {/* if there is an icon, show the icon*/}
             {Icon && <Icon className="headerOption__icon" />}
+            {/* if in the props there is avatar, so show the avatar*/}
             {avatar && <Avatar className="headerOption__icon" src={avatar} />}
             <h3 className="headerOption__title">{title}</h3>
         </div>
