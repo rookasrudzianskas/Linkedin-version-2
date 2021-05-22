f73c80c6dcc888234fe9cbbbe1c18ff9bd658c8a
Rokas <rookas.rudzenskas@gmail.com>
2021-05-22 08:37:03 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import "./styles/Header.css";
 import SearchIcon from '@material-ui/icons/Search';
-
+import HomeIcon from '@material-ui/icons/Home';
+import PeopleIcon from '@material-ui/icons/People';
+import WorkIcon from '@material-ui/icons/Work';
+import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import HeaderOption from "./HeaderOption";
+import {Avatar} from "@material-ui/core";
 
 const Header = () => {
     return (
@@ -16,7 +23,12 @@ const Header = () => {
             </div>
 
             <div className="header__right">
-
+                <HeaderOption title="Home" Icon={HomeIcon} />
+                <HeaderOption title="My Network" Icon={PeopleIcon} />
+                <HeaderOption title="Jobs" Icon={WorkIcon} />
+                <HeaderOption title="Messaging" Icon={ChatBubbleIcon} />
+                <HeaderOption title="Notifications" Icon={NotificationsIcon} />
+                <HeaderOption avatar="https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK_400x400.jpg" title="me" />
             </div>
         </div>
     );
