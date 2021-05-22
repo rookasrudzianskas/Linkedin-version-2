9e42ce72fd4bd70f337d5d1f620a5543f04c3e0e
Rokas <rookas.rudzenskas@gmail.com>
2021-05-22 18:32:32 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
 
         <div className="app__body">
             <Sidebar />
+            <Feed />
         </div>
     {/*    App body     */}
 
