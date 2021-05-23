c18483747f26db6d985c669eb8efbe8fbfb9e235
Rokas <rookas.rudzenskas@gmail.com>
2021-05-23 16:51:24 +0300

    Rokas

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,6 +3,9 @@ import "./styles/Feed.css";
 import CreateIcon from '@material-ui/icons/Create';
 import InputOption from "./InputOption";
 import ImageIcon from '@material-ui/icons/Image';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import NoteIcon from '@material-ui/icons/Note';
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 
 const Feed = () => {
     return (
@@ -18,6 +21,9 @@ const Feed = () => {
 
                 <div className="feed__inputOptions">
                     <InputOption Icon={ImageIcon} color="#79B5F9" title="Photo" />
+                    <InputOption Icon={SubscriptionsIcon} color="#E7A33E" title="Video" />
+                    <InputOption Icon={NoteIcon} color="#C0CBCD" title="Event" />
+                    <InputOption Icon={CalendarTodayIcon} color="#7FC15E" title="Write an article" />
                 </div>
             </div>
         </div>
