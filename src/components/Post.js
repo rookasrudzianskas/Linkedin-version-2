cf82039fe2815ceb85f1e2fb4829d997153d4ea7
Rokas <rookas.rudzenskas@gmail.com>
2021-05-24 16:09:05 +0300

    Rokas

--- src/components/Post.js
+++ src/components/Post.js
@@ -8,7 +8,7 @@ const Post = ({name, description, message, photoUrl}) => {
     return (
         <div className="post">
             <div className="post__header">
-                <Avatar />
+                <Avatar src={photoUrl} >{name[0]}</Avatar>
                 <div className="post__info">
                     <h2>{name}</h2>
                     <p>{description}</p>
