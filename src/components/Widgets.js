2e303d4072692a949b2ad3b232080dfd8fe097a2
Rokas <rookas.rudzenskas@gmail.com>
2021-05-24 16:32:12 +0300

    Rokas

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import "./styles/Widgets.css";
-import {InfoOutlined} from "@material-ui/icons";
+import {FiberManualRecord, InfoOutlined} from "@material-ui/icons";
 
 const Widgets = () => {
-    return (
+
+    const newsArticle = (heading, subtitle) => (
+        <div className="widgets__article">
+            <div className="widgets__articleLeft">
+                <FiberManualRecord />
+            </div>
+
+            <div className="widgets__articleRight">
+                <h4>{heading}</h4>
+                <p>{subtitle}</p>
+            </div>
+        </div>
+    )
+     return (
         <div className="widgets">
             <div className="widgets__header">
                 <h2>LinkedIn News</h2>
                 <InfoOutlined />
 
             </div>
+            {newsArticle("Rokas REACT IS BACK!", "Top News - 9999 readers")}
+            {newsArticle("Rokas REACT IS BACK!", "Top News - 9999 readers")}
+            {newsArticle("Rokas REACT IS BACK!", "Top News - 9999 readers")}
+            {newsArticle("Rokas REACT IS BACK!", "Top News - 9999 readers")}
+            {newsArticle("Rokas REACT IS BACK!", "Top News - 9999 readers")}
+            {newsArticle("Rokas REACT IS BACK!", "Top News - 9999 readers")}
+            {newsArticle("Rokas REACT IS BACK!", "Top News - 9999 readers")}
+            {newsArticle("Rokas REACT IS BACK!", "Top News - 9999 readers")}
+            {newsArticle("Rokas REACT IS BACK!", "Top News - 9999 readers")}
+            {newsArticle("Rokas REACT IS BACK!", "Top News - 9999 readers")}
+            {newsArticle("lorem10", "Top News - 9999 readers")}
         </div>
+
     );
 };
 
