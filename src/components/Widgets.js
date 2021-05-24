33a31467cc68d74cb992f76020f46fb9ad1b852e
Rokas <rookas.rudzenskas@gmail.com>
2021-05-24 16:22:34 +0300

    Rokas

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import "./styles/Widgets.css";
+import {InfoOutlined} from "@material-ui/icons";
 
 const Widgets = () => {
     return (
         <div className="widgets">
+            <div className="widgets__header">
+                <h2>LinkedIn News</h2>
+                <InfoOutlined />
 
+            </div>
         </div>
     );
 };
